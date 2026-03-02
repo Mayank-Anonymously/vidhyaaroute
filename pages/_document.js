894ae5d3ff3fac48ae882dde9588f169dfe16a02
Mayank-Anonymously <craftaar0@gmal.com>
2026-03-02 23:34:18 +0530

    added analytics tag

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,23 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+	return (
+		<Html lang='en'>
+			<Head />
+			<script
+				async
+				src='https://www.googletagmanager.com/gtag/js?id=G-79M9RYF2X8'></script>
+			<script>
+				{`window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-79M9RYF2X8');`}
+			</script>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
 }
